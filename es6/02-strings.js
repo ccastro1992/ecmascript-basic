1070c9bda7aaec0a684432abb75a07007de3c5de
ccastro1992 <cristhianc10@hotmail.com>
2023-07-08 22:18:36 -0500

    Strings en ES6

---
+++ es6/02-strings.js
@@ -0,0 +1,16 @@
+// Strings
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+
+// Template Literals
+let epicPhrase2 = `${hello} ${world} !`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'Esto es un string \n' + 'esto es otra linea';
+let lorem2 = `Esto es una frase
+    la continuacion de la frase
+`;
+
+console.log(lorem2);
