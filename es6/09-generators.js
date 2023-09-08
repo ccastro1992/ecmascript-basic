7d4ceb20939cd7735514c9eea0941044465ea853
Cristhian Castro <criscastrog10@gmail.com>
2023-09-08 11:10:42 -0500

    [IMP] Clase de generators

--- es6/09-generators.js
+++ es6/09-generators.js
@@ -1,6 +1,6 @@
 
 function* iterate(array) {
-    for (let value of array){
+    for (let value of array) {
         yield value;
     }
 }
@@ -10,3 +10,23 @@ console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
+
+
+
+function* getId() {
+    // Tu código aquí 👈
+    let michiIds = []
+    while (true) {
+        let new_id = Math.floor(Math.random() * (100 - 1) + 1);
+        if (!michiIds.includes(new_id)) {
+            michiIds.push(new_id);
+            yield new_id;
+        }
+    }
+}
+
+const id = getId()
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
