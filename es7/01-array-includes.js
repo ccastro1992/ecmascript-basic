35216e4adde72e57966722c8c53b0fa1bba51081
Cristhian Castro <criscastrog10@gmail.com>
2023-09-08 11:39:45 -0500

    [IMP] Clase de includes ES7

---
+++ es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [2,3,4,6,8,1]
+
+console.log(numbers.includes(2));
+console.log(numbers.includes(0));
+
+let names = ['Cris']
+
+console.log(names.includes('Cris'));
+console.log(names.includes('Juan'));
