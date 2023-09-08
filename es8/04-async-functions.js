95a541ea0198f115c3a9e5d96e1233b38e084552
Cristhian Castro <criscastrog10@gmail.com>
2023-09-08 14:55:59 -0500

    [IMP] Async Functions ES8

--- es8/04-async-functions.js
+++ es8/04-async-functions.js
@@ -15,3 +15,18 @@ const anotherfn = async () => {
 console.log('Before');
 anotherfn();
 console.log('After')
+
+//////////////// TEST 2
+
+const fn_async_2 = () => {
+    setTimeout(() => console.log('TEST TIMEOUT'), 2000)
+    // console.log('TEST TIMEOUT')
+}
+
+const anotherfn_2 = async() => {
+    await fn_async_2();
+}
+
+console.log('Before');
+fn_async_2();
+console.log('After')
