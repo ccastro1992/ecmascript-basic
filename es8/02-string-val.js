0d09f31388ecdca8470cf8328a5b21b4fef687b8
Cristhian Castro <criscastrog10@gmail.com>
2023-09-08 12:03:07 -0500

    [IMP] String Val / Trailing Commas}

---
+++ es8/02-string-val.js
@@ -0,0 +1,4 @@
+const str = 'Hello';
+console.log(str.padStart(10, '0'));
+console.log(str.padEnd(10, '0'));
+
---
+++ es8/03-trailing-commas.js
@@ -0,0 +1,5 @@
+const array_numbers = [9,2,3,4,5,6,7,8,9,,,,85];
+
+console.log(array_numbers);
+console.log(array_numbers.length);
+
